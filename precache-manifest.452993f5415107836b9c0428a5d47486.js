03063242dfe3ef262c40d0166c479763d388a566
Jesse Strode <jesse_strode@condenast.com>
2020-04-20 17:30:57 -0400

    Updates

---
+++ precache-manifest.452993f5415107836b9c0428a5d47486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96abc3e0d60baebc9674ea256c1bf6aa",
+    "url": "/weightedProConList/index.html"
+  },
+  {
+    "revision": "01355739f50029eecb38",
+    "url": "/weightedProConList/static/css/main.828c3d76.chunk.css"
+  },
+  {
+    "revision": "c639b7bb2900744d8a9f",
+    "url": "/weightedProConList/static/js/2.bf82d165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weightedProConList/static/js/2.bf82d165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01355739f50029eecb38",
+    "url": "/weightedProConList/static/js/main.bcea48e5.chunk.js"
+  },
+  {
+    "revision": "cfdcc260d95a2d81a9aa",
+    "url": "/weightedProConList/static/js/runtime-main.91d88aba.js"
+  }
+]);
